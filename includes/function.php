a9e142758beabd4cd089817a8708ff95818dafab
JosueGarcia04 <josueadrian042006@gmail.com>
2026-02-21 22:16:33 -0600

    cambios en la sesion

--- includes/function.php
+++ includes/function.php
@@ -1,4 +1,7 @@
 <?php
+$tiempo_vida = 60 * 60 * 24 * 30;
+ini_set('session.cookie_lifetime', $tiempo_vida);
+ini_set('session.gc_maxlifetime', $tiempo_vida);
 session_start();
 if (!isset($_SESSION['products'])) {
     $_SESSION['products'] = [];
@@ -6,7 +9,7 @@ if (!isset($_SESSION['products'])) {
 function getProductIndex($id) {
     foreach ($_SESSION['products'] as $index => $product) {
         if ($product['id'] === $id) {
-            return $index;
+    return $index;
         }
     }
     return -1;
